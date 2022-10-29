8b471c195b2e7f4f0b3f0f4bcf24d291039b291c
Darshan0811 <19comp.darshan.shah@gmail.com>
2022-10-29 12:48:01 +0530

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,31 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("awesome app"),
       ),
-      body: Center(
-        child: Container(
-          //color: Colors.red,
-          height: 100,
-          width: 100,
-          child: Text("Hello there", style: TextStyle(color: Colors.purple)),
-          padding: const EdgeInsets.all(
-              4), //const etle value pachi kyay badlaavi na joye.
-          //Child ni andar na child mate padding margin ni jem kaam kare.Margin aaya nai kaam kare etle.
+      body: Container(
           alignment: Alignment.center,
-          //Upar color j comment karyo a simply aakhu box ne red kari nakhe.Pan gradient aapvo hoy to box decoration use thase.Decoration etle sajaavu etle koi pan vastu(aaya box) ne game te rite sajaavu hoy to decoration use thai.
-          decoration: BoxDecoration(
-              shape: BoxShape
-                  .rectangle, //shape aapva mate.By default box square hoy etle rectangle karyu.
-              //Shape,borderRadius,borderShadow savthi pela j kaam karse gradient pachi aapva jais to nai kare.
-              borderRadius: BorderRadius.circular(10),
-              //box ma shadow aapva mate.Syntax important che.
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black,
-                  blurRadius: 5,
-                ),
-              ],
-              color: Colors.red,
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
-        ),
-      ),
+          color: Colors.cyan,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //main axis aaya row che etle aadi(x axis thase).
+            //Cross axis etle opposite axis aaya vertical etle y-axis thase.
+            children: <Widget>[
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              )
+            ],
+          )),
     );
   }
 }
